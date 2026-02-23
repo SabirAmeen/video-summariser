99f05905ed5645531a296376f0321834cf1ce61e
sabir <sabir.ameen93@gmail.com>
2026-02-23 21:26:45 +0530

    dummy commit

--- src/components/UploadSection/index.tsx
+++ src/components/UploadSection/index.tsx
@@ -33,6 +33,7 @@ export function UploadSection({
       }
       const responseObj = await response.json();
       setVideoId(responseObj.blobName);
+      console.log(responseObj.blobName);
       setUploadStatus("Upload successful!");
     } catch (error) {
       setUploadStatus(
